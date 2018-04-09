eab8c67f6777d727a8771286506210945eb0cb2e
frosin <frosin@list.ru>
2018-04-09 11:56:55 +0300

    added partners-office and index.php

---
+++ partners-office/index.php
@@ -0,0 +1,22 @@
+<?
+require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
+$APPLICATION->SetTitle("Партнерский кабинет");
+?><?$APPLICATION->IncludeComponent(
+	"custom:partners_products",
+	"",
+	Array(
+		"AJAX_MODE" => "Y",
+		"AJAX_OPTION_ADDITIONAL" => "",
+		"AJAX_OPTION_HISTORY" => "N",
+		"AJAX_OPTION_JUMP" => "N",
+		"AJAX_OPTION_STYLE" => "Y",
+		"CACHE_TIME" => "36000000",
+		"CACHE_TYPE" => "A",
+		"ELEMENTS_COUNT" => "2",
+		"ELEMENT_ID" => "ELEMENT_ID",
+		"IBLOCK_ID" => "2",
+		"IBLOCK_TYPE" => "products",
+		"PARTNERS_IBLOCK_ID" => "5",
+		"PARTNER_ID" => "PARTNER_ID"
+	)
+);?><br><?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
